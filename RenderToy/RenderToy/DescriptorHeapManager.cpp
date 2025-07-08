9f8ad800464e85639a974696b777b069df56be54
zeming22.l <zeming22.l@outlook.com>
2025-07-09 00:51:15 +0800

    Finished initial version of descriptor heap manager

---
+++ RenderToy/RenderToy/DescriptorHeapManager.cpp
@@ -0,0 +1,284 @@
+#include "DescriptorHeapManager.h"
+
+DescriptorHeapManager::DescriptorHeapManager(ID3D12Device* pDevice, UINT adapterNodeMask)
+    : m_pDevice(pDevice), m_adapterNodeMask(adapterNodeMask)
+{
+}
+
+bool DescriptorHeapManager::Initialize()
+{
+    if (m_initialized)
+    {
+        return true;
+    }
+
+    if (!m_pDevice)
+    {
+        return false;
+    }
+
+    // Get desc sizes of all types.
+    UINT m_rtvDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    UINT m_dsvDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+    UINT m_cbvSrvUavDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+    UINT m_samplerDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER);
+
+    // Create Rtv heap, this heap is used for all render targets, and swapchain buffers
+    D3D12_DESCRIPTOR_HEAP_DESC heapDesc;
+    heapDesc.NumDescriptors = m_defaultRtvDescriptorHeapSize;
+    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    heapDesc.NodeMask = m_adapterNodeMask;
+    if (FAILED(m_pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(m_rtvDescriptorHeap.GetAddressOf()))))
+    {
+        return false;
+    }
+
+    // Create Dsv heap, this heap is used for all render depth buffer
+    heapDesc.NumDescriptors = m_defaultDsvDescriptorHeapSize;
+    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+    if (FAILED(m_pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(m_dsvDescriptorHeap.GetAddressOf()))))
+    {
+        return false;
+    }
+
+    // Create sampler state descriptor heap, this heap is directly for shader usage
+    heapDesc.NumDescriptors = m_samplerDescriptorSize;
+    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER;
+    heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+    if (FAILED(m_pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(m_samplerDescriptorHeap.GetAddressOf()))))
+    {
+        return false;
+    }
+
+    // Create cbv/srv/uav descriptor heap (ring buffer), this is directly used by shaders.
+    heapDesc.NumDescriptors = m_defaultRingBufferSize;
+    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+    heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+    if (FAILED(m_pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(m_cbvSrvUavRingBuffer.GetAddressOf()))))
+    {
+        return false;
+    }
+
+    // Initialize cbv/srv/uav id list, which indicates the position of gpu non-visible of the corresponding idx. 
+    // (lower 32bit represents the index of vector, higher 32 bit represents the index of element within that vector, 0xffffffffffffffff means invalid)
+    m_cbvSrvUavRingBufferIds = std::vector<uint64_t>(m_defaultRingBufferSize, UINT64_MAX);
+
+    m_initialized = true;
+    return true;
+}
+
+uint64_t DescriptorHeapManager::CreateRenderTargetView(ID3D12Resource* pResource, const D3D12_RENDER_TARGET_VIEW_DESC* pDesc)
+{
+    if (m_currentRtvCount >= m_defaultRtvDescriptorHeapSize)
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(m_rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), m_currentRtvCount, m_rtvDescriptorSize);
+    m_pDevice->CreateRenderTargetView(pResource, pDesc, rtvHeapHandle);
+
+    return m_currentRtvCount++;
+}
+
+uint64_t DescriptorHeapManager::CreateDepthStencilView(ID3D12Resource* pResource, const D3D12_DEPTH_STENCIL_VIEW_DESC* pDesc)
+{
+    if (m_currentDsvCount >= m_defaultDsvDescriptorHeapSize)
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE dsvHeapHandle(m_dsvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), m_currentDsvCount, m_dsvDescriptorSize);
+    m_pDevice->CreateDepthStencilView(pResource, pDesc, dsvHeapHandle);
+
+    return m_currentDsvCount++;
+}
+
+uint64_t DescriptorHeapManager::CreateSampler(const D3D12_SAMPLER_DESC* pDesc)
+{
+    if (m_currentSamplerCount >= m_defaultSamplerDescriptorHeapSize)
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE samplerHeapHandle(m_samplerDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), m_currentSamplerCount, m_samplerDescriptorSize);
+    m_pDevice->CreateSampler(pDesc, samplerHeapHandle);
+
+    return m_currentSamplerCount++;
+}
+
+bool DescriptorHeapManager::AddNewCbvSrvUavDescriptorHeap()
+{
+    if (m_cbvSrvUavDescriptorPositions.empty() || m_cbvSrvUavDescriptorPositions.back().size() >= m_defaultCbvSrvUavDescriptorHeapSize)
+    {
+        // Need to create a new shader non visible descriptor heap
+        D3D12_DESCRIPTOR_HEAP_DESC heapDesc;
+        heapDesc.NumDescriptors = m_defaultCbvSrvUavDescriptorHeapSize;
+        heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+        heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        heapDesc.NodeMask = m_adapterNodeMask;
+        ComPtr<ID3D12DescriptorHeap> newDescriptorHeap = nullptr;
+        if (FAILED(m_pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(newDescriptorHeap.GetAddressOf()))))
+        {
+            return false;
+        }
+
+        m_cbvSrvUavDescriptorHeaps.push_back(newDescriptorHeap);
+        m_cbvSrvUavDescriptorPositions.push_back(std::vector<int>());
+    }
+
+    return true;
+}
+
+uint64_t DescriptorHeapManager::GetIdOfCbvSrvUav(uint64_t arrayIdx, uint64_t elementIdx)
+{
+    uint64_t id = 0;
+
+    id = id | arrayIdx << 32;
+    id = id | elementIdx;
+
+    return id;
+}
+
+void DescriptorHeapManager::GetArrayIdxAndElementIdxByViewId(uint64_t viewId, uint64_t& arrayIdx, uint64_t& elementIdx)
+{
+    arrayIdx = viewId >> 32;
+    elementIdx = viewId << 32;
+    elementIdx = elementIdx >> 32;
+}
+
+uint64_t DescriptorHeapManager::CreateConstantBufferView(const D3D12_CONSTANT_BUFFER_VIEW_DESC* pDesc)
+{
+    if (!AddNewCbvSrvUavDescriptorHeap())
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE heapHandle(m_cbvSrvUavDescriptorHeaps.back()->GetCPUDescriptorHandleForHeapStart(), (UINT)m_cbvSrvUavDescriptorPositions.back().size(), m_cbvSrvUavDescriptorSize);
+    m_cbvSrvUavDescriptorPositions.back().push_back(INT32_MAX);
+    m_pDevice->CreateConstantBufferView(pDesc, heapHandle);
+
+    // Figure id of the cbv/srv/uav
+    return GetIdOfCbvSrvUav(m_cbvSrvUavDescriptorHeaps.size() - 1, m_cbvSrvUavDescriptorPositions.back().size() - 1);
+}
+
+uint64_t DescriptorHeapManager::CreateShaderResourceView(ID3D12Resource* pResource, const D3D12_SHADER_RESOURCE_VIEW_DESC* pDesc)
+{
+    if (!AddNewCbvSrvUavDescriptorHeap())
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE heapHandle(m_cbvSrvUavDescriptorHeaps.back()->GetCPUDescriptorHandleForHeapStart(), (UINT)m_cbvSrvUavDescriptorPositions.back().size(), m_cbvSrvUavDescriptorSize);
+    m_cbvSrvUavDescriptorPositions.back().push_back(INT32_MAX);
+    m_pDevice->CreateShaderResourceView(pResource, pDesc, heapHandle);
+
+    // Figure id of the cbv/srv/uav
+    return GetIdOfCbvSrvUav(m_cbvSrvUavDescriptorHeaps.size() - 1, m_cbvSrvUavDescriptorPositions.back().size() - 1);
+}
+
+uint64_t DescriptorHeapManager::CreateUnorderedAccessView(ID3D12Resource* pResource, ID3D12Resource* pCounterResource, const D3D12_UNORDERED_ACCESS_VIEW_DESC* pDesc)
+{
+    if (!AddNewCbvSrvUavDescriptorHeap())
+    {
+        return UINT64_MAX;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE heapHandle(m_cbvSrvUavDescriptorHeaps.back()->GetCPUDescriptorHandleForHeapStart(), (UINT)m_cbvSrvUavDescriptorPositions.back().size(), m_cbvSrvUavDescriptorSize);
+    m_cbvSrvUavDescriptorPositions.back().push_back(INT32_MAX);
+    m_pDevice->CreateUnorderedAccessView(pResource, pCounterResource, pDesc, heapHandle);
+
+    // Figure id of the cbv/srv/uav
+    return GetIdOfCbvSrvUav(m_cbvSrvUavDescriptorHeaps.size() - 1, m_cbvSrvUavDescriptorPositions.back().size() - 1);
+}
+
+bool DescriptorHeapManager::BindCbvSrvUavToPipeline(uint64_t viewId, D3D12_GPU_DESCRIPTOR_HANDLE& outGpuDescriptorHandle)
+{
+    // Figure of arrayIdx and elementIdx from id
+    uint64_t arrayIdx = 0;
+    uint64_t elementIdx = 0;
+    GetArrayIdxAndElementIdxByViewId(viewId, arrayIdx, elementIdx);
+
+    if (m_cbvSrvUavDescriptorPositions.size() <= arrayIdx || m_cbvSrvUavDescriptorPositions[arrayIdx].size() <= elementIdx)
+    {
+        return false;
+    }
+    
+    // The view has already binded, early out
+    if (m_cbvSrvUavDescriptorPositions[arrayIdx][elementIdx] != INT_MAX)
+    {
+        CD3DX12_GPU_DESCRIPTOR_HANDLE gpuHeapHandle(m_cbvSrvUavRingBuffer->GetGPUDescriptorHandleForHeapStart(), m_cbvSrvUavDescriptorPositions[arrayIdx][elementIdx], m_cbvSrvUavDescriptorSize);
+        outGpuDescriptorHandle = gpuHeapHandle;
+
+        return true;
+    }
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE cpuHeapHandleSrc(m_cbvSrvUavDescriptorHeaps[arrayIdx]->GetCPUDescriptorHandleForHeapStart(), (INT)elementIdx, m_cbvSrvUavDescriptorSize);
+    CD3DX12_CPU_DESCRIPTOR_HANDLE cpuHeapHandleDst(m_cbvSrvUavRingBuffer->GetCPUDescriptorHandleForHeapStart(), m_ringBufferItr, m_cbvSrvUavDescriptorSize);
+    m_pDevice->CopyDescriptorsSimple(
+        1,
+        cpuHeapHandleSrc,
+        cpuHeapHandleDst,
+        D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+    // Update the non shader visible heap pointer to ring buffer itr
+    m_cbvSrvUavDescriptorPositions[arrayIdx][elementIdx] = m_ringBufferItr;
+
+    if (m_cbvSrvUavRingBufferIds[m_ringBufferItr] != UINT64_MAX)
+    {
+        // Clear the position of corresponding non shader visible heap if needed
+        GetArrayIdxAndElementIdxByViewId(m_cbvSrvUavRingBufferIds[m_ringBufferItr], arrayIdx, elementIdx);
+        m_cbvSrvUavDescriptorPositions[arrayIdx][elementIdx] = INT_MAX;
+    }
+
+    // Update viewId of ring buffer pos
+    m_cbvSrvUavRingBufferIds[m_ringBufferItr] = viewId;
+
+    CD3DX12_GPU_DESCRIPTOR_HANDLE gpuHeapHandle(m_cbvSrvUavRingBuffer->GetGPUDescriptorHandleForHeapStart(), m_ringBufferItr, m_cbvSrvUavDescriptorSize);
+    outGpuDescriptorHandle = gpuHeapHandle;
+
+    // advance the iterator
+    m_ringBufferItr = (m_ringBufferItr + 1) % m_defaultRingBufferSize;
+
+    return true;
+}
+
+DescriptorHeapManager::~DescriptorHeapManager()
+{
+    if (m_rtvDescriptorHeap)
+    {
+        m_rtvDescriptorHeap->Release();
+        m_rtvDescriptorHeap = nullptr;
+    }
+
+    if (m_dsvDescriptorHeap)
+    {
+        m_dsvDescriptorHeap->Release();
+        m_dsvDescriptorHeap = nullptr;
+    }
+
+    if (m_samplerDescriptorHeap)
+    {
+        m_samplerDescriptorHeap->Release();
+        m_samplerDescriptorHeap = nullptr;
+    }
+
+    if (m_cbvSrvUavRingBuffer)
+    {
+        m_cbvSrvUavRingBuffer->Release();
+        m_cbvSrvUavRingBuffer = nullptr;
+    }
+
+    for (auto& descHeap : m_cbvSrvUavDescriptorHeaps)
+    {
+        if (descHeap)
+        {
+            descHeap->Release();
+            descHeap = nullptr;
+        }
+    }
+
+    m_cbvSrvUavDescriptorHeaps.clear();
+    m_cbvSrvUavDescriptorPositions.clear();
+    m_cbvSrvUavRingBufferIds.clear();
+}
--- RenderToy/RenderToy/DescriptorHeapManager.h
+++ RenderToy/RenderToy/DescriptorHeapManager.h
@@ -2,31 +2,114 @@
 
 #include "Includes.h"
 
+/// <summary>
+/// A descriptor heap manager, which keeps view creation and binding abstract
+/// </summary>
 class DescriptorHeapManager
 {
 private:
-	const UINT m_defaultSrvUavDescriptorHeapSize = 128;
+	const UINT m_defaultCbvSrvUavDescriptorHeapSize = 64;
 
 	const UINT m_defaultRtvDescriptorHeapSize = 64;
 
 	const UINT m_defaultDsvDescriptorHeapSize = 64;
 
-	UINT m_srvDescriptorSize = 0;
+	const UINT m_defaultSamplerDescriptorHeapSize = 64;
 
-	UINT m_dsvDescriptorSize = 0;
+	const UINT m_defaultRingBufferSize = 1024;
 
 	UINT m_rtvDescriptorSize = 0;
 
-	std::vector<ComPtr<ID3D12DescriptorHeap>> m_srvUavDescriptorHeaps;
+	UINT m_dsvDescriptorSize = 0;
+
+	UINT m_cbvSrvUavDescriptorSize = 0;
+
+	UINT m_samplerDescriptorSize = 0;
+
+	UINT m_currentRtvCount = 0;
+
+	UINT m_currentDsvCount = 0;
+
+	UINT m_currentSamplerCount = 0;
 
 	ComPtr<ID3D12DescriptorHeap> m_rtvDescriptorHeap = nullptr;
 
 	ComPtr<ID3D12DescriptorHeap> m_dsvDescriptorHeap = nullptr;
 
-public:
-	DescriptorHeapManager();
+	ComPtr<ID3D12DescriptorHeap> m_samplerDescriptorHeap = nullptr;
 
-	bool Initialize(ID3D12Device* pDevice);
+	ComPtr<ID3D12DescriptorHeap> m_cbvSrvUavRingBuffer = nullptr;
+
+	std::vector<ComPtr<ID3D12DescriptorHeap>> m_cbvSrvUavDescriptorHeaps;
+
+	std::vector<std::vector<int>> m_cbvSrvUavDescriptorPositions;
+
+	std::vector<uint64_t> m_cbvSrvUavRingBufferIds;
+
+	UINT m_ringBufferItr = 0;
+
+	ID3D12Device* m_pDevice = nullptr;
+
+	UINT m_adapterNodeMask = 0;
+
+	bool m_initialized = false;
+
+public:
+	DescriptorHeapManager(ID3D12Device* pDevice, UINT adapterNodeMask);
+
+	bool Initialize();
+
+	/// <summary>
+	/// Create an rtv,
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateRenderTargetView(ID3D12Resource* pResource, const D3D12_RENDER_TARGET_VIEW_DESC* pDesc);
+
+	/// <summary>
+	/// Create a dsv
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateDepthStencilView(ID3D12Resource* pResource, const D3D12_DEPTH_STENCIL_VIEW_DESC* pDesc);
+
+	/// <summary>
+	/// Create sampler
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateSampler(const D3D12_SAMPLER_DESC* pDesc);
+
+	/// <summary>
+	/// Create constant buffer view
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateConstantBufferView(const D3D12_CONSTANT_BUFFER_VIEW_DESC* pDesc);
+
+	/// <summary>
+	/// Create shader resource view
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateShaderResourceView(ID3D12Resource* pResource, const D3D12_SHADER_RESOURCE_VIEW_DESC* pDesc);
+
+	/// <summary>
+	/// Create unordered access view
+	/// </summary>
+	/// <returns>id</returns>
+	uint64_t CreateUnorderedAccessView(ID3D12Resource* pResource, ID3D12Resource* pCounterResource, const D3D12_UNORDERED_ACCESS_VIEW_DESC* pDesc);
+
+	/// <summary>
+	/// Bind the view of given id to current pipeline
+	/// </summary>
+	bool BindCbvSrvUavToPipeline(uint64_t viewId, D3D12_GPU_DESCRIPTOR_HANDLE& outGpuDescriptorHandle);
 
 	~DescriptorHeapManager();
+
+
+private:
+	// Add a new cbv/srv/uav non shader visible descriptor heap when needed.
+	bool AddNewCbvSrvUavDescriptorHeap();
+
+	// Get the id of cbv/srv/uav of given non shader visible descriptor heap.
+	uint64_t GetIdOfCbvSrvUav(uint64_t arrayIdx, uint64_t elementIdx);
+
+	// Get the array idx and element idx given viewId
+	void GetArrayIdxAndElementIdxByViewId(uint64_t viewId, uint64_t& arrayIdx, uint64_t& elementIdx);
 };
